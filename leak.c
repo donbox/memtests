f2d32f06884285850f0101c7450eba882ee07de6
Don Box <donbox@ymail.com>
2021-12-12 13:53:53 -0800

    Updated to memset the block after allocation

--- leak.c
+++ leak.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main() {
+int main(int argc, char** argv) {
+    const size_t size = 1024 * 1024;
     for (int i = 0; 1; i++) {
         printf("%d\n", i);
-        void *p = calloc(1024 * 1024, sizeof(char));
-        if (p == 0) {
+        void *p = malloc(size);
+        if (p != 0) { 
+            memset(p, 0xFF, size);
+        } else {
             printf("Failed after %d MB allocations, press any key to exit\n", i);
             char c = getchar();
             return 1;
